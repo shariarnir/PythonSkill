af3156e9550abad5891d9ad7366a60ea7f9ffbd8
Md.Shariar Hossain <shariar5532@gmail.com>
2019-07-13 11:51:54 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+'''
+def Subtract (a, b):
+    return (a-b)
+print(Subtract(10,12))
+print(Subtract(15,6))
+'''
+s = sum([5,15,2])
+print(s)
+mx = max(15,6)
+print( mx )
---
+++ method.py
@@ -0,0 +1,10 @@
+'''
+class ABC :
+    def method_abc (self):
+        print("I am in method_abc of ABC class. ")
+class_ref = ABC()
+class_ref.method_abc()
+'''
+import math
+ceil_val = math.ceil(15.25)
+print("Ceiling value of 15.25 is : ", ceil_val)
